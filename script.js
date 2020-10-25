e766ad4b3927de8aaf6cf6cd639d9e5a35da216e
Molly Durst <mollymadegraphics@yahoo.com>
2020-10-25 18:13:42 -0400

    created placeholder prompts

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-<<<<<<< HEAD
-/*define variables - lowercase, uppercase, special characters and numbers
-*/
+//define variables - lowercase, uppercase, special characters and numbers
 
 var lower = 'abcdefghijklmnopqrstuvwxyz';
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var special = '!@#$^&%*()+=-[]{}|:<>?,.';
 var numbers = '1234567890';
 
-console.log(lower, upper, special, numbers);
+//generate random password
 
 /*function to generate a random password
 */
 
 /*prompt user to input desired character length - between 8 and 128
 */
+window.alert("What length would you like the password to be? (Between 8 to 128 characters)");
+
+/*inform the user that a series of prompts are coming
+*/
+window.alert("Please answer the next 4 questions to personalize your password");
+
 
 /*character type prompts: lowercase, uppercase, special characters, numbers
 */
+window.confirm("Question 1/4: Would you like your password to include lower case letters?");
+window.confirm("Question 2/4: Would you like your password to include upper case letters?");
+window.confirm("Question 3/4: Would you like your password to include special characters?");
+window.confirm("Question 4/4: Would you like your password to include numbers?");
 
 /*remind user to select at least one character type from the prompts
 */
-
-/*generate random password
-*/
+window.alert("Please select at least one character type from the prompts)");
 
 /*function to copy the new password to the clipboard
 */
@@ -42,7 +48,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-=======
+
 /*define variables - lowercase, uppercase, special characters and numbers
 */
 
@@ -79,4 +85,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
->>>>>>> 3e1334fa3a6a3ee638b600be1288e9862e7bef7e
